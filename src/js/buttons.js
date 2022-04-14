467688074681132d25790dac9eae4d3745d9a391
JoshuaHawatta <joshuahawattaazevedo@gmail.com>
2022-04-14 18:48:37 -0300

    created buttons module and tested it

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,10 +1,11 @@
-const nextQuestionBtn = document.querySelector('#next-btn');
-const allRadios = document.querySelectorAll('.btn');
+const nextQuestionButton = document.querySelector('#next-btn');
 
-export const disableNextQuestionBtn = element => {
-  return element.disabled ? element.disabled = false : element.disabled = true;
-};
-
-export const uncheckRadios = () => allRadios.forEach(radio => radio.checked = false);
-
-export const disableRadios = trueOrFalse => allRadios.forEach(radio => radio.disabled = trueOrFalse);
+export const invertDisabledAttribute = () => {
+  if(!nextQuestionButton.disabled) {
+    nextQuestionButton.disabled = true;
+    return 'Escolha uma resposta.'
+  }
+  
+  nextQuestionButton.disabled = false;
+  return 'Próxima pergunta!'
+}
