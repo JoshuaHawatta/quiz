2bbc0ab3f45d839df9e1f532954310003cd7c4cd
JoshuaHawatta <joshuahawattaazevedo@gmail.com>
2022-04-15 18:09:19 -0300

    changed answers options

--- src/js/questions.js
+++ src/js/questions.js
@@ -1,45 +1,35 @@
-export const questions = [
+const questions = [
   {
-    question: "Quem inventou o Javascript?",
-    options: {
-      a: "Apple",
-      b: "Netscape",
-      c: "Google"
-    },
-    answer: "b",
-    img: "../assets/images/img-one.jpg"
+    question: 'Quem inventou o Javascript?',
+    options: ['Apple', 'Netscape', 'Google'],
+    answer: 'Netscape',
+    img: '../assets/images/img-one.jpg'
   },
 
   {
-    question: "Steve Jobs criou o primeiro computador Apple.",
-    options: {
-      a: "Quem fez isso foi Steve Wozniak.",
-      b: "Claro, ele era o criador da empresa.",
-      c: "A Apple não faz computadores."
-    },
-    answer: "a",
-    img: "../assets/images/img-two.png"
+    question: 'Steve Jobs criou o primeiro computador Apple.',
+    options: [
+      'Quem fez isso foi Steve Wozniak.', 
+      'Claro, ele era o criador da empresa.', 
+      'A Apple não faz computadores.'
+    ],
+    answer: 'Quem fez isso foi Steve Wozniak.',
+    img: '../assets/images/img-two.png'
   },
 
   {
-    question: "Qual foi a empresa que criou o React.js?",
-    options: {
-      a: "Microsoft.",
-      b: "Uber.",
-      c: "Facebook."
-    },
-    answer: "c",
-    img: "../assets/images/img-three.png"
+    question: 'Qual foi a empresa que criou o React.js?',
+    options: ['Microsoft.', 'Uber.', 'Facebook.'],
+    answer: 'Facebook.',
+    img: '../assets/images/img-three.png'
   },
-
+  
   {
-    question: "Qual é o nível de experiência entre programadores?",
-    options: {
-      a: "Júnior, Pleno, Sênior.",
-      b: "CEO, Sênior, Pleno.",
-      c: "Estagiário, Pleno, Júnior."
-    },
-    answer: "a",
-    img: "../assets/images/img-four.jpg"
+    question: 'Quais são os níveis de experiência na programação?',
+    options: ['Júnior, Pleno, Sênior.', 'CEO, Sênior, Pleno.', 'Estagiário, Pleno, Júnior.'],
+    answer: 'Júnior, Pleno, Sênior.',
+    img: '../assets/images/img-four.jpg'
   }
 ];
+
+export default questions;
