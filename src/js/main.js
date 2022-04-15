9449ca06e210833d51fa064efe9126d4c39bfd15
JoshuaHawatta <joshuahawattaazevedo@gmail.com>
2022-04-15 17:56:52 -0300

    implementing part of the logic on main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
-import { invertDisabledAttribute } from "./buttons.js";
+import questions from './questions.js';
+import originalCounters from './counters.js';
+import showActualQuestion from './actual-question.js';
+
+const counters = originalCounters(questions.lenght);
+
+counters.counter !== questions.length ? showActualQuestion() : console.log('IMPLEMENT_LOGIC...');
