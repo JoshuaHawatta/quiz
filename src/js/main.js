94e9b931b9d926093577f975aeb7e3c108fd1f06
JoshuaHawatta <joshuahawattaazevedo@gmail.com>
2022-04-15 22:19:08 -0300

    main.js is now responsible to show what will be seen in screen.

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,22 @@
 import questions from './questions.js';
+import { invertDisabledAttribute } from './buttons.js';
 import originalCounters from './counters.js';
-import showActualQuestion from './actual-question.js';
 
-const counters = originalCounters(questions.lenght);
+const body = document.querySelector('body');
+const questionTitle = document.querySelector('#question');
+const image = document.querySelector('#img');
+const answers = document.querySelectorAll('.parag');
+const nextQuestionButton = document.querySelector('#next-btn');
+const counters = originalCounters();
 
-counters.counter !== questions.length ? showActualQuestion() : console.log('IMPLEMENT_LOGIC...');
+const showActualQuestion = () => {
+  nextQuestionButton.textContent = invertDisabledAttribute();
+
+  questionTitle.textContent = questions[counters.counter].question;
+  image.src = questions[counters.counter].img;
+  questions[counters.counter].options.map((option, index) => answers[index].textContent = option);
+};
+
+const endQuiz = () => body.innerHTML = `<p>Você acertou ${counters.showResults()} perguntas!</p>`
+
+counters.counter !== questions.length ? showActualQuestion() : endQuiz();
