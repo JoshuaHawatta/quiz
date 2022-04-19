b63df895d962fcc2a4e12700a196cb97b23b991d
JoshuaHawatta <joshuahawattaazevedo@gmail.com>
2022-04-19 02:44:59 -0300

    finished main.js logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,35 @@
 import questions from './questions.js';
-import { invertDisabledAttribute } from './buttons.js';
-import originalCounters from './counters.js';
+import handleChoosenAnswer, { invertDisabledAttribute } from './buttons.js';
+import counterInstance from './counters.js';
+export const counters = counterInstance(); // IMPORT_IT_ON_'buttons.js'_TO_ACSSES_THIS_SAME_INSTANCE
 
 const body = document.querySelector('body');
 const questionTitle = document.querySelector('#question');
 const image = document.querySelector('#img');
 const answers = document.querySelectorAll('.parag');
+const answerOptions = document.querySelectorAll('.radios');
 const nextQuestionButton = document.querySelector('#next-btn');
-const counters = originalCounters();
 
-const showActualQuestion = () => {
+const actualQuestion = () => {
   nextQuestionButton.textContent = invertDisabledAttribute();
 
   questionTitle.textContent = questions[counters.counter].question;
   image.src = questions[counters.counter].img;
-  questions[counters.counter].options.map((option, index) => answers[index].textContent = option);
-};
+  questions[counters.counter].options.map((option, index) => answers[index].textContent = option)
+}
 
-const endQuiz = () => body.innerHTML = `<p>Você acertou ${counters.showResults()} perguntas!</p>`
+const finish = () => body.innerHTML = `
+  <div className='quiz-end-div'>
+    <p>Você acertou ${counters.showResults()} perguntas!</p>
+  </div>`
 
-counters.counter !== questions.length ? showActualQuestion() : endQuiz();
+const continueOrEndQuiz = () => counters.counter !== questions.length ? actualQuestion() : finish();
+
+nextQuestionButton.addEventListener('click', () => {
+  counters.incrementCounter();
+  answerOptions.forEach(element => element.disabled = false);
+  continueOrEndQuiz()
+})
+
+actualQuestion();
+handleChoosenAnswer();
